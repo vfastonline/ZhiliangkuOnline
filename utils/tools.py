480c4f07651ff120071a620bf4f97e9a217245ff
xuhuiliang-maybe <15011544971@163.com>
2018-09-10 18:10:21 +0800

    增加工具方法注释

--- utils/tools.py
+++ utils/tools.py
@@ -3,17 +3,18 @@ import json
 
 from bson import ObjectId
 
-# 手机号码正则表达式
+# 手机号码'正则表达式
 REGEX_MOBILE = "^1[358]\d{9}$|^147\d{8}$|^176\d{8}$"
 
 
+# 重写json解码，适应mongodb，ObjectId
 class JSONEncoder(json.JSONEncoder):
 	def default(self, o):
-		print(o, "======")
 		if isinstance(o, ObjectId):
 			return str(o)
 		return json.JSONEncoder.default(self, o)
 
 
+# 猴子补丁，扩展json解码
 def monkey_patch_jsonencoder():
 	json.JSONEncoder = JSONEncoder
