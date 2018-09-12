447476e988ed17f7865cbfffca2642020e490bb9
xuhuiliang-maybe <15011544971@163.com>
2018-09-12 21:06:37 +0800

    增加线上 mongodb  redis  部署启动说明

--- apps/project/models.py
+++ apps/project/models.py
@@ -1,6 +1,4 @@
 #!encoding:utf-8
-# !encoding:utf-8
-from __future__ import unicode_literals
 
 from django.core.validators import MinValueValidator
 
@@ -13,7 +11,7 @@ class ProjectTechnology(BaseModelMixin):
 	项目'技术方向
 	"""
 	name = models.CharField(max_length=50, verbose_name="名称", help_text="技术方向名称")
-	desc = models.TextField('简介', default='', blank=True, null=True)
+	desc = models.TextField(verbose_name='简介', blank=True)
 	video = models.ForeignKey("Video", verbose_name='总考核', related_name='Technology', blank=True, null=True,
 							  limit_choices_to={'type': 3}, help_text=u"针对本技术方向下所有项目的总考核", on_delete=models.CASCADE)
 
@@ -30,7 +28,8 @@ class Project(models.Model):
 	"""项目说明书"""
 	name = models.CharField('名称', max_length=50)
 	desc = models.TextField('简介', max_length=1000, blank=True, null=True, default='')
-	technology = models.ForeignKey(Technology, verbose_name="技术分类", blank=True, null=True, on_delete=models.CASCADE)
+	technology = models.ForeignKey(ProjectTechnology, verbose_name="技术分类", blank=True, null=True,
+								   on_delete=models.CASCADE)
 	sequence = models.PositiveIntegerField('顺序', default=1, validators=[MinValueValidator(1)], help_text="技术分类下显示顺序")
 	is_lock = models.BooleanField("锁定", default=True)
 	home_show = models.BooleanField("首页展示", default=False)
