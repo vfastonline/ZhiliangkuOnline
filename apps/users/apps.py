7f100a16877eb9df1ba823b8310fdec6a4b12d1f
xuhuiliang-maybe <15011544971@163.com>
2018-09-17 12:10:29 +0800

    重构保存用户信号机制

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -5,8 +5,6 @@ from django.apps import AppConfig
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_migrate, post_save
 
-User = get_user_model()
-
 
 def init_user_profile_role(sender, verbosity, **kwargs):
 	"""初始化-用户角色信息
@@ -24,10 +22,11 @@ def init_user_profile_role(sender, verbosity, **kwargs):
 		traceback.print_exc()
 
 
-def post_save_user(sender, instance=None, created=False, **kwargs):
+def create_auth_token(sender, instance=None, created=False, **kwargs):
 	# 是否新建，因为update的时候也会进行post_save
 	from user_resumes.models import UserResume
 	from users.models import Role
+
 	if created:
 		# 第三方接口创建用户后，增加默认学生角色，创建默认简历
 		instance.roles.add(Role.objects.get(index=0))
@@ -42,4 +41,5 @@ class UsersConfig(AppConfig):
 
 	def ready(self):
 		post_migrate.connect(init_user_profile_role, sender=self)
-		post_save.connect(post_save_user, sender=User)
+		User = get_user_model()
+		post_save.connect(create_auth_token, sender=User)
