6ec7c4418a91b4b0f38db0ee2ebce0e3da94a9b8
hzj <huangzj@chuzhi.cn>
2018-09-19 20:21:00 +0800

    add header

--- front/src/components/home/heder.vue
+++ front/src/components/home/heder.vue
@@ -1,12 +1,12 @@
 <template>
-	<div>
-		<ul>
+	<div class="content">
+		<ul class="list_one">
 			<li>LOGO</li>
 			<li>首页</li>
 			<li>项目</li>
 			<li>社区</li>
 		</ul>
-		<ul>
+		<ul class="list_tow">
 			<li><input type="" name=""></li>
 			<span>登录</span>
 			<span>/</span>
@@ -15,8 +15,58 @@
 	</div>
 </template>
 <style type="text/css" lang="scss" scoped>
+* {
+	padding: 0;
+	margin: 0;
+}
+ .content {
+ 	width:1200px;
+ 	height: 50px;
+ 	margin: 0 auto;
+ 	background: #eee;
+ }
+ .content .list_one {
+    float: left;
+ 	width: 700px;
+ 	height: 50px;
+ }
+ .content .list_one li {
+ 	float: left;
+ 	width: 100px;
+ 	height: 50px;
+ 	line-height: 50px;
+ 	text-align: center;
+ }
+ .content .list_one li:hover{
+ 	background: #c4adff;
+ }
+ .content .list_tow {
+ 	float: right;
+ 	width: 500px;
+ 	height: 50px;
+ }
+.content .list_tow {
+  
+}
+.content .list_tow li {
+	float: left;
+	
+}
+.content .list_tow li input {
+	width: 200px;
+	height: 20px;
+	position: relative;
+	top: 10px;
+}
+.content .list_tow span {
+	float: right;
+	width: 24px;
+	position: relative;
+	top: 10px;
+	right: 15px;
+	color: #000;
 
-
+}
 </style>
 <script type="text/javascript">
 export default {
