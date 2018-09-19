9036ff8bee5d15ecd2e08dab45d20844302a6ac0
xuhuiliang-maybe <15011544971@163.com>
2018-09-19 17:37:16 +0800

    修改查询技术方向信息视图

--- apps/tracks_learning/routers.py
+++ apps/tracks_learning/routers.py
@@ -3,6 +3,4 @@
 from tracks_learning.technology_info import TechnologyListInfo
 from utils.tools import router
 
-
-router.register(r'technology_list_info', TechnologyListInfo, base_name="user_resume")#技术分类
-# router.register(r'projects_info')
+router.register(r'technology', TechnologyListInfo, base_name="user_resume")
