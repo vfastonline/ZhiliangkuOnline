647be66beaed21922eb73c6ed3c9084ba0441378
xuhuiliang-maybe <15011544971@163.com>
2018-09-20 09:48:27 +0800

    banner增加缓存机制

--- apps/banner/views.py
+++ apps/banner/views.py
@@ -2,21 +2,21 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework_extensions.cache.mixins import CacheResponseMixin
 
 from .serializers import *
 
 
-class BannerViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class BannerViewSet(CacheResponseMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
 	"""
 	list:
-	Return a list of all the existing users.
+		获取banner信息
 	"""
 	queryset = Banner.objects.all()
 	serializer_class = BannerSerializer
 	authentication_classes = ()
 	permission_classes = ()
 
-	# 常用过滤器之DjangoFilterBackend, SearchFilter, OrderingFilter
 	filter_backends = (DjangoFilterBackend,)
 
 	filter_fields = ("category",)
