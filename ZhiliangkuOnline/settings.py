d06a10fbac240521b3949ba81a8c66495398a75d
xuhuiliang-maybe <15011544971@163.com>
2018-09-21 10:54:49 +0800

    增加第三方认证自定义管道

--- ZhiliangkuOnline/settings.py
+++ ZhiliangkuOnline/settings.py
@@ -302,6 +302,21 @@ SOCIAL_AUTH_WEIXIN_SECRET = ''
 
 # 第三方登录成功后跳转页面
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/index/'
+SOCIAL_AUTH_FIELDS_STORED_IN_SESSION = ['key']
+
+# 自定义管道
+SOCIAL_AUTH_PIPELINE = (
+	'social_core.pipeline.social_auth.social_details',
+	'social_core.pipeline.social_auth.social_uid',
+	'social_core.pipeline.social_auth.auth_allowed',
+	'social_core.pipeline.social_auth.social_user',
+	'social_core.pipeline.user.get_username',
+	'social_core.pipeline.user.create_user',
+	'utils.social_pipeline.save_profile_team',
+	'social_core.pipeline.social_auth.associate_user',
+	'social_core.pipeline.social_auth.load_extra_data',
+	'social_core.pipeline.user.user_details',
+)
 
 # 创建日志的路径
 LOG_PATH = os.path.join(BASE_DIR, 'log')
