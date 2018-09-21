a34f69170b9e6c03e83d2ceec2dcc98960887d56
xuhuiliang-maybe <15011544971@163.com>
2018-09-21 12:04:49 +0800

    自定义管道

--- utils/social_pipeline.py
+++ utils/social_pipeline.py
@@ -10,10 +10,7 @@ def save_user_team(strategy, details, backend, user, response, *args, **kwargs):
 	invitations_code = strategy.session_get('invitations_code')
 	print(invitations_code, '===========')
 	if invitations_code:
-		profile = user.get_profile()
 		team = Team.objects.filter(code=invitations_code)
 		if team.exists():
-			if profile is None:
-				profile = User(user_id=user.id)
-			profile.team.add(team[0])
-			profile.save()
+			user.team.add(team[0])
+			user.save()
