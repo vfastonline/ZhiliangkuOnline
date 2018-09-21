ca7aebb0b8d31e5bb715aee0af267b9654a2d188
xuhuiliang-maybe <15011544971@163.com>
2018-09-21 13:23:38 +0800

    注释没开发的app

--- apps/tracks_learning/routers.py
+++ apps/tracks_learning/routers.py
@@ -1,6 +1,6 @@
 #!encoding:utf-8
 
-from tracks_learning.technology_info import TechnologyListInfo
-from utils.tools import router
-
-router.register(r'technology', TechnologyListInfo, base_name="user_resume")
+# from tracks_learning.technology_info import TechnologyListInfo
+# from utils.tools import router
+#
+# router.register(r'technology', TechnologyListInfo, base_name="user_resume")
