36513381c60a65ce93edf4b926caa9a019806a63
xuhuiliang-maybe <15011544971@163.com>
2018-09-30 17:42:41 +0800

    去掉限速

--- ZhiliangkuOnline/settings.py
+++ ZhiliangkuOnline/settings.py
@@ -205,15 +205,15 @@ REST_FRAMEWORK = {
 	'PAGE_SIZE': 10,
 	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 
-	# 截流
-	'DEFAULT_THROTTLE_CLASSES': (
-		'rest_framework.throttling.AnonRateThrottle',
-		'rest_framework.throttling.UserRateThrottle'
-	),
-	'DEFAULT_THROTTLE_RATES': {
-		'anon': '1000/day',
-		'user': '1000/day',
-	}
+	# # 截流
+	# 'DEFAULT_THROTTLE_CLASSES': (
+	# 	'rest_framework.throttling.AnonRateThrottle',
+	# 	'rest_framework.throttling.UserRateThrottle'
+	# ),
+	# 'DEFAULT_THROTTLE_RATES': {
+	# 	'anon': '1000/day',
+	# 	'user': '1000/day',
+	# }
 
 }
 
