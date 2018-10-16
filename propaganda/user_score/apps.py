71ce03a5875a06a15041a9ef51d7597fdf8627c0
caochao <caochao_mx@163.com>
2018-10-16 11:48:36 +0800

    初始化评价项

--- propaganda/user_score/apps.py
+++ propaganda/user_score/apps.py
@@ -1,4 +1,35 @@
+# encoding: utf-8
+import traceback
+
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def init_score_item(sender, verbosity, **kwargs):
+	"""
+	初始化-得分项
+	:param sender:
+	:param kwargs:
+	:return:
+	"""
+	try:
+		from .models import ScoreItem
+
+		score_items = [
+			{"name": "精神面貌", "desc": "xx", "rule": "2"},
+			{"name": "课堂气氛", "desc": "xx", "rule": "2"},
+			{"name": "知识引导", "desc": "xx", "rule": "2"},
+			{"name": "理解程度", "desc": "xx", "rule": "1"},
+			{"name": "操作程度", "desc": "xx", "rule": "1"},
+
+		]
+
+		[ScoreItem.objects.get_or_create(**score_item) for score_item in score_items]
+
+		if verbosity == 1:
+			print(" 初始化-得分项 OK")
+	except:
+		traceback.print_exc()
 
 
 class UserScoreConfig(AppConfig):
@@ -7,4 +38,4 @@ class UserScoreConfig(AppConfig):
 	main_menu_index = 23
 
 	def ready(self):
-		pass
+		post_migrate.connect(init_score_item, sender=self)
