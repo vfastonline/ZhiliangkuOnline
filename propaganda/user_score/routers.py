f89df00cbb24da252bb4eb5f8611748156b0736c
caochao <caochao_mx@163.com>
2018-10-18 18:30:32 +0800

    注释接口

--- propaganda/user_score/routers.py
+++ propaganda/user_score/routers.py
@@ -3,10 +3,9 @@
 from utils.tools import router
 from .add_user_score import AddUserScoreViewSet
 from .date_range_user_score import *
-from .get_score_item import GetScoreItemViewSet
 from .today_has_user_score_record import TodayHasUserScoreRecordViewSet
 
-router.register(r'get_score_item', GetScoreItemViewSet, base_name="get_score_item")
+# router.register(r'get_score_item', GetScoreItemViewSet, base_name="get_score_item")
 router.register(r'today_has_user_score_record', TodayHasUserScoreRecordViewSet, base_name="today_has_user_score_record")
 router.register(r'add_user_score', AddUserScoreViewSet, base_name="add_user_score")
 
